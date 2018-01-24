ef10a369c33da0e960c6995c095b66c41717fb92
Samyak Upadhyay <samyak.upadhyay123@gmail.com>
2018-01-24 12:42:36 +0530

    changed the database handler class

--- app/src/main/java/com/example/samyakupadhyay/appscommunication/DatabaseHandler.java
+++ app/src/main/java/com/example/samyakupadhyay/appscommunication/DatabaseHandler.java
@@ -38,8 +38,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
-        onCreate(db);
+
     }
 
     void addContact(Contact contact) {
@@ -87,19 +86,6 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         return contactList;
     }
 
-    // Updating single contact
-    public int updateContact(Contact contact) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put(KEY_NAME, contact.getName());
-        values.put(KEY_PH_NO, contact.getPhoneNumber());
-
-        // updating row
-        return db.update(TABLE_CONTACTS, values, KEY_ID + " = ?",
-                new String[] { String.valueOf(contact.getID()) });
-    }
-
     // Deleting single contact
     public void deleteContact(Contact contact) {
         SQLiteDatabase db = this.getWritableDatabase();
@@ -109,14 +95,4 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     }
 
 
-    // Getting contacts Count
-    public int getContactsCount() {
-        String countQuery = "SELECT  * FROM " + TABLE_CONTACTS;
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery(countQuery, null);
-        cursor.close();
-
-        // return count
-        return cursor.getCount();
-    }
 }
